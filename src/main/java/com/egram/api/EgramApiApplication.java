e8bc431af5a328c659d327280dbc7698c4e47eb9
Ram Chadar <61204888+ram-chadar@users.noreply.github.com>
2025-11-09 13:36:32 +0530

    dots

--- src/main/java/com/egram/api/EgramApiApplication.java
+++ src/main/java/com/egram/api/EgramApiApplication.java
@@ -10,6 +10,6 @@ public class EgramApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgramApiApplication.class, args);
-		System.err.println("App started..");
+		System.err.println("App started.....");
 	}
 }
